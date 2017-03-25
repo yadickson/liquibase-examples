9b008bee0eda4b4b2618d9bc3850fdb437d38bb6
Yadickson Soto <yadickson@gmail.com>
2017-03-25 00:56:37 -0400

    Se incorpora el sp encargado de insertar vehiculo

---
+++ IntegracionWD/src/main/resources/db/1.0.0/scripts/SP_INSERTAR_VEHICULOS.sql
@@ -0,0 +1,54 @@
+CREATE PROCEDURE [dbo].[SP_INSERTAR_VEHICULOS]
+     @Patente [nchar](10),
+     @Marca [nvarchar](50),
+     @Modelo [nvarchar](50),
+     @Anio [nchar](10),
+     @TipoVehiculo nvarchar(50),
+     @Contrato nvarchar(50),
+     @RazonSocial [nvarchar](50),
+     @FechaExpiracion [nchar](10),
+     @MotivoRechazo [nvarchar](4000),
+     @Resultado nvarchar (50) OUTPUT,
+     @Mensaje nvarchar(4000) OUTPUT
+AS
+BEGIN TRY
+
+     INSERT INTO [dbo].[Vehiculos]
+     (
+	      [Patente],
+	      [Marca],
+	      [Modelo],
+	      [Anio],
+	      [TipoVehiculo],
+	      [Contrato],
+	      [RazonSocial],
+	      [FechaExpiracion],
+	      [MotivoRechazo]
+	 )
+	 VALUES
+	 (
+	      @Patente,
+	      @Marca,
+	      @Modelo,
+	      @Anio,
+	      @TipoVehiculo,
+	      @Contrato,
+	      @RazonSocial,
+	      @FechaExpiracion,
+	      @MotivoRechazo
+	 );
+	 
+	 IF @@trancount > 0 COMMIT TRANSACTION;
+	 
+	 SET @Resultado = 'EXITOSO';
+	 SET @Mensaje = 'OK';
+
+END TRY
+BEGIN CATCH
+
+     IF @@trancount > 0 ROLLBACK TRANSACTION;
+
+     SET @Resultado = 'FALLIDO';
+     SET @Mensaje = ERROR_PROCEDURE() + ' '+ ERROR_MESSAGE();
+
+END CATCH
