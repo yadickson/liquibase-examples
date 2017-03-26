ebaedc9cf670a63e1c45e5bdfff4da9cc472e4d6
Yadickson Soto <yadickson.soto@basedos.cl>
2017-03-25 22:12:01 -0300

    Se agrega sp base para consulta de identificador

---
+++ IntegracionWD/src/main/resources/db/1.0.0/scripts/SP_CONSULTA_IDENTIFICADOR.sql
@@ -0,0 +1,27 @@
+CREATE PROCEDURE [dbo].[SP_CONSULTA_IDENTIFICADOR]
+	@Identificador [varchar](10),
+	@Tipo [varchar](1),
+	@Id [BIGINT] OUTPUT,
+	@Resultado [varchar](10) OUTPUT,
+	@Mensaje [nvarchar](4000) OUTPUT
+AS
+BEGIN TRY
+	SET @Id = 0;	
+
+  	/*
+  	 EXEC SP_GET_IDENTIFICADOR @Identificador, @Tipo, @Id OUTPUT;
+  	*/
+
+	IF @@trancount > 0 COMMIT TRANSACTION;
+	SET @Resultado = 'EXITOSO';
+	SET @Mensaje = 'OK';
+	  
+END TRY
+BEGIN CATCH
+	
+	IF @@trancount > 0 ROLLBACK TRANSACTION;
+	SET @Resultado = 'FALLIDO';
+	SET @Mensaje = ERROR_PROCEDURE() + ' '+ ERROR_MESSAGE(); 
+	
+END CATCH
+
