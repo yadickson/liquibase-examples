893bec80b2f0f9237ba96cf9fcf34ab9e9fdddc2
Yadickson Soto <yadickson.soto@basedos.cl>
2017-03-26 09:08:46 -0300

    Se agrega mecanismo base para pruebas de consulta de transito

--- IntegracionWD/src/main/resources/db/1.0.0/scripts/ConsultaTransito.sql
+++ IntegracionWD/src/main/resources/db/1.0.0/scripts/ConsultaTransito.sql
@@ -3,16 +3,33 @@ CREATE PROCEDURE [dbo].[ConsultaTransito]
 	@FechaHasta [varchar](8),
 	@Identificador [varchar](10),
 	@Tipo [varchar](1),
-	@Listado CURSOR VARYING OUTPUT,
 	@Resultado [varchar](10) OUTPUT,
 	@Mensaje [nvarchar](4000) OUTPUT
 AS
 BEGIN TRY
 
+	CREATE TABLE #temp
+	(
+	    [ID] [int] IDENTITY(1,1) NOT NULL,
+	    [NAME1] [nvarchar](15),
+	    [NAME2] [nvarchar](15),
+	    [NAME3] [nvarchar](15),
+	    [NAME4] [nvarchar](15),
+	    [NAME5] [nvarchar](15)
+	)
+
+	INSERT INTO #temp ([NAME1], [NAME2], [NAME3], [NAME4], [NAME5]) VALUES ('Test1','Test2','Test3','Test4','Test5')
+	INSERT INTO #temp ([NAME1], [NAME2], [NAME3], [NAME4], [NAME5]) VALUES ('Test1','Test2','Test3','Test4','Test5')
+
+	SELECT [NAME1],[NAME2],[NAME3],[NAME4],[NAME5] FROM #temp
+
+	DROP TABLE #temp
+
 	IF @@trancount > 0 COMMIT TRANSACTION;
+
 	SET @Resultado = 'EXITOSO';
 	SET @Mensaje = 'OK';
-	  
+
 END TRY
 BEGIN CATCH
 	
